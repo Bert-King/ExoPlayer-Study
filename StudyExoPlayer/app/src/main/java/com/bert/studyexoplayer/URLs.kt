33df3c7040a91682ca882fc3f8816fc4f9f3ad11
Bertking <kingcheeng163@gmail.com>
2020-09-07 20:51:49 +0800

    引入ExoPlayer-UI模块...

---
+++ StudyExoPlayer/app/src/main/java/com/bert/studyexoplayer/URLs.kt
@@ -0,0 +1,16 @@
+package com.bert.studyexoplayer
+
+/**
+ *
+ * @Author: bertking
+ * @ProjectName: StudyExoPlayer
+ * @CreateAt: 2020/9/7 8:38 PM
+ * @UpdateAt: 2020/9/7 8:38 PM
+ * @UpdateRemark: 更新说明
+ * @Version: 1.0
+ * @Description:
+ */
+object URLs {
+    const val mp4Url = "https://video.kts.g.mi.com/super/ktv_1282851653_U1hJa05ZWkJqcnF0NWlXckZSWEZTdz09_1598584360284.mp4"
+    const val m3u8Url = "https://mivideo.g.mi.com/91eec2b713d65e76967a374d15b35ae0_720.m3u8"
+}
