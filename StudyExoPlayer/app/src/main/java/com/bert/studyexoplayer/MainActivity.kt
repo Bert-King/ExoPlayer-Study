12bc19f3afc00fd88e15ff8c077343f4bf89f1c6
Bertking <kingcheeng163@gmail.com>
2020-09-08 20:50:17 +0800

    Update MainActivity.kt

--- StudyExoPlayer/app/src/main/java/com/bert/studyexoplayer/MainActivity.kt
+++ StudyExoPlayer/app/src/main/java/com/bert/studyexoplayer/MainActivity.kt
@@ -1,64 +1,89 @@
 package com.bert.studyexoplayer
 
+import android.content.Intent
 import android.net.Uri
-import android.os.Bundle
-import androidx.appcompat.app.AppCompatActivity
-import com.google.android.exoplayer2.C
+import android.util.Log
 import com.google.android.exoplayer2.SimpleExoPlayer
-import com.google.android.exoplayer2.drm.DrmSessionManager
-import com.google.android.exoplayer2.source.MediaSource
 import com.google.android.exoplayer2.source.ProgressiveMediaSource
 import com.google.android.exoplayer2.upstream.DataSource
 import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory
 import com.google.android.exoplayer2.util.Util
 import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
-    val TAG = MainActivity::class.java.simpleName
+/**
+ * evictor:驱逐者，清除者，收回者，常见于英文关于Cache的文档
+ */
+class MainActivity : BaseActivity(R.layout.activity_main) {
 
     lateinit var simpleExoPlayer: SimpleExoPlayer
-    lateinit var dataSourceFactory:DataSource.Factory
+    lateinit var dataSourceFactory: DataSource.Factory
 
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
+    override fun init() {
         simpleExoPlayer = SimpleExoPlayer.Builder(this).build()
 
-        player_view?.player = simpleExoPlayer
+        dataSourceFactory =
+            DefaultDataSourceFactory(this, Util.getUserAgent(this, "Study-ExoPlayer"))
 
-
-        dataSourceFactory = DefaultDataSourceFactory(this,Util.getUserAgent(this,"Study-ExoPlayer"))
-
-        val mp4MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory).createMediaSource(Uri.parse(URLs.mp4Url))
-        simpleExoPlayer.prepare(mp4MediaSource)
-        simpleExoPlayer.playWhenReady= true
-
+        initPlayer()
     }
 
+    override fun initViews() {
+        btn_cache?.setOnClickListener {
+            startActivity(Intent(context, CacheActivity::class.java))
+        }
+    }
 
-
+    private fun initPlayer() {
+        val mp4MediaSource = ProgressiveMediaSource.Factory(dataSourceFactory)
+            .createMediaSource(Uri.parse(URLs.mp4Url))
+        simpleExoPlayer.prepare(mp4MediaSource)
+        simpleExoPlayer.playWhenReady = true
+        player_view?.player = simpleExoPlayer
+    }
 
 
     override fun onResume() {
         super.onResume()
+        Log.d(TAG, "------onResume-------")
+        if (Util.SDK_INT <= 23) {
+            initPlayer()
+            player_view?.onResume()
+        }
+
     }
 
     override fun onStart() {
         super.onStart()
+        Log.d(TAG,"------onStart-------")
+        if (Util.SDK_INT > 23) {
+            initPlayer()
+            player_view?.onResume()
+        }
     }
 
     override fun onPause() {
         super.onPause()
+        Log.d(TAG, "------onPause-------")
+
+        if (Util.SDK_INT <= 23) {
+            player_view?.onPause()
+//            simpleExoPlayer.release()
+        }
+
     }
 
     override fun onStop() {
         super.onStop()
+        Log.d(TAG, "------onStop-------")
+        if (Util.SDK_INT > 23) {
+            player_view?.onPause()
+//            simpleExoPlayer.release()
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
+        Log.d(TAG, "------onDestroy-------")
+        player_view?.overlayFrameLayout?.removeAllViews()
     }
 }
